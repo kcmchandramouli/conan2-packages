7b7a7d716e26d8a1b1fdb881c5279d8e61bcf163
Chandra Mouli <chandramouli7616@gmail.com>
2025-03-08 13:08:44 +0530

    restructuring

---
+++ recipes/7z/binary/conanfile.py
@@ -0,0 +1,20 @@
+from conan import ConanFile
+from conan.tools.files import get, copy
+
+required_conan_version = ">=2.10.0"
+
+class ApacheMavenConan(ConanFile):
+    name = "apache-maven"
+    version = "3.9.9"
+    license = ("Apache License 2.0")
+    homepage = "https://maven.apache.org/download.cgi"
+    description = "Apache Maven is a software project management and comprehension tool."
+    topics = ("maven", "build", "java")
+    settings = "os", "arch"
+
+    def build(self):
+        get(self, **self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)],
+            destination=self.source_folder, strip_root=True)
+        
+    def package(self):
+        copy(self, "*", src=self.source_folder, dst=self.package_folder)
